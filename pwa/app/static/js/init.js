7ec0352ae27665e14ad0011d7ef21f56f7b6877d
Swarup Kr. Gupta <56894217+swarup-cloud@users.noreply.github.com>
2020-03-28 21:10:01 +0530

    PWA

---
+++ pwa/app/static/js/init.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // nav menu
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, {edge: 'right'});
+  // add recipe form
+  const forms = document.querySelectorAll('.side-form');
+  M.Sidenav.init(forms, {edge: 'left'});
+});
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
